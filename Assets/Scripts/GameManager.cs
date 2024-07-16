8915e21f76ad1c8ea505ee2fac93529a90bc7cbf
youjin43 <wlwlfdl3324@naver.com>
2024-07-16 12:26:53 +0900

    간단하게 UI 적용시킴

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,18 +9,37 @@ using UnityEngine.SceneManagement;//씬 관련 라이브러리
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject runningOverBoard; //가게 운영이 끝났을때 활성화 할 오브젝트 
+    //싱글턴으로 만들어야 하는디... 우선 ㄱ
 
+
+    [Header("Data")] 
+    [SerializeField] private int money;
+    [SerializeField] private int popularity;
+
+
+    [Header("About Running")]
+    [SerializeField] private float runTime; // 가게 runTime시간
+    [SerializeField] private bool isRunning; //가게가 운영중인지 표시하는 변수
+
+    [Header("About UI")]
+    public GameObject runningOverBoard; //가게 운영이 끝났을때 활성화 할 오브젝트 
     public Text runningTimeText;//현재 시간을 표시할 텍스트 컴포넌트 
     public Text moneyText;//현재 돈을 표시할 텍스트 컴포넌트
-
-    private float runTime; // 가게 운runTime시간
-    private bool isRunning; //가게가 운영중인지 표시하는 변수 
+    public Text popularityText;//현재 돈을 표시할 텍스트 컴포넌트
 
     void Start()
     {
-        runTime = 0;
-        isRunning = true;
+        //이전 데이터 가져오기
+        money = PlayerPrefs.GetInt("money"); //주어진 키로 저장된 값이 없으면 기본값을 반환
+        popularity = PlayerPrefs.GetInt("popularity");
+
+        //데이터 표시 
+        moneyText.text = "Money : " + money;
+        popularityText.text = "Popularity : " + popularity;
+
+        //가게 운영 시작 -> start는 게임 처음 실행될때 실행되는거라 가게 운영 시작하는 기능은 따로 빼야할 듯
+        StartRunning();
+
     }
 
     void Update()
@@ -33,25 +52,38 @@ public class GameManager : MonoBehaviour
 
             if (runTime>=10.0f) // 우선 10으로 해놓음 
             {
-                isRunning = false;
+                EndRunning();
             }
         }
         else
         {
-            //가게 운영이 끝났을때 처리할것들
-
-            // 정산 화면 뜨고 -> runningOverBoard 활성화
-
             //우선 임시로 R 누르면 넘어가도록 해놓음 
             if (Input.GetKeyDown(KeyCode.R))
             {
-                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene("Farm");
             }
         }
     }
 
-    public void EndRunning()
+
+    //가게 운영 시작하는 함수 
+    private void StartRunning()
     {
-        //가게 운영이 끝났을때 호출 할 함수 구현은 안하고 우선 선언만 해둠
+        runTime = 0;
+        isRunning = true;
+    }
+
+    //가게 운영이 끝났을때 호출 할 함수
+    private void EndRunning()
+    {
+        isRunning = false; //운영 끝! 
+        runningOverBoard.SetActive(true); // 정산 화면 뜨기
+
+        //정산 결과 저장
+        //PlayerPrefs.SetFloat(string key, float value); //float 값을 저장할때 : 키와 키와 대응하는 값을 입력값으로 받음
+        //PlayerPrefs.GetFloat(string key); float 값을 불러올 때
+        //int 와 string도 가능
+
+
     }
 }
