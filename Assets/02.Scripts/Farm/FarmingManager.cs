f613d74260fec21bec1e9c84eca2daab1be3c087
youjin43 <wlwlfdl3324@naver.com>
2024-08-07 17:58:40 +0900

    Revert ">???"
    This reverts commit 72f390c4e61af37f9960a36245464ddc18b88345.

--- Assets/02.Scripts/Farm/FarmingManager.cs
+++ Assets/02.Scripts/Farm/FarmingManager.cs
@@ -158,10 +158,7 @@ public class FarmingManager : MonoBehaviour
     private string filePath; // 데이터 저장 경로..
     public SaveData saveFarmingData = new SaveData();
 
-<<<<<<< HEAD
 >>>>>>> parent of 8486beb (FarmDataSave)
-=======
->>>>>>> parent of eafa279 (Merge pull request #52 from youjin-43/Inventory)
     
 
     public void SaveFarmingData()
@@ -230,10 +227,7 @@ public class FarmingManager : MonoBehaviour
         File.WriteAllText(farmingDataFilePath, json);
 =======
         File.WriteAllText(filePath, json);
-<<<<<<< HEAD
 >>>>>>> parent of 8486beb (FarmDataSave)
-=======
->>>>>>> parent of eafa279 (Merge pull request #52 from youjin-43/Inventory)
     }
 
 
@@ -253,7 +247,6 @@ public class FarmingManager : MonoBehaviour
             string json = File.ReadAllText(path);
             Debug.Log(json);
 <<<<<<< HEAD
-<<<<<<< HEAD
             Dictionary<PosInt, SaveFarmingData> tempDic = DictionaryJsonUtility.FromJson<PosInt, SaveFarmingData>(json);
 =======
 
@@ -263,14 +256,6 @@ public class FarmingManager : MonoBehaviour
 >>>>>>> parent of 8486beb (FarmDataSave)
             Debug.Log(tempDic.Count + "!!!!!!!!!!!!!!!!!!!!1");
 
-=======
-
-            SaveData saveData = JsonUtility.FromJson<SaveData>(json);
-            Dictionary<PosInt, SaveFarmingData> tempDic = saveData.saveData;
-
-            Debug.Log(tempDic.Count + "!!!!!!!!!!!!!!!!!!!!1");
-
->>>>>>> parent of eafa279 (Merge pull request #52 from youjin-43/Inventory)
             foreach (var item in tempDic)
             {
                 tempDic[item.Key].PrintData();
