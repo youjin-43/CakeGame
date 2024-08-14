1469b29597e01e2a5afd314b93085bc5816d6f08
youjin43 <wlwlfdl3324@naver.com>
2024-08-14 16:32:09 +0900

    Cake SO 수정
    아 파일 잘못골랐다

--- Assets/02.Scripts/CakeStore(SW)/CakeSO.cs
+++ Assets/02.Scripts/CakeStore(SW)/CakeSO.cs
@@ -6,8 +6,8 @@ public class CakeSO : ItemSO
     public int cakeCost;
     public int bakeTime;
     public int cakePrice;
-    public int[] materialIdxs;
-    public int[] materialCounts;
+    public int[] materialIdxs = new int[5];
+    public int[] materialCounts = new int[5]; // 최대로 설정할 수 있는 재료의 갯수를 우선 5개로 해놓음 ;
     public int cakeCount;
     public bool isLocked;
     public int cakeIdx;
